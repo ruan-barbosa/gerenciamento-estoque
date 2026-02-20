8cf92fecd621301047e23c2f83720785a12c846c
ruan-barbosa <ruanrodriguesbarbosa08@gmail.com>
2026-02-19 21:27:40 -0300

    Aula 3: Criar classe

---
+++ modelos.py
@@ -0,0 +1,21 @@
+"""
+Produtos
+
+Atributos:
+* entrada: saber quando um produto entrou no estoque.
+* produto: para saber o que está sendo colocado no sistema.
+* código: para diferenciar os produtos.
+* quantidade: para saber quantos de cada um há.
+* saida: saber quando o produto saiu do estoque
+
+Comportamento:
+* inserir_produto: inserir produto ao estoque.
+* remover_produto: retirar produto do estoque.
+* conferir_produto: conferir qualidade do produto.
+* cadastrar_pedido: atribuir produto a um pedido.
+* entrega_produto: entregar produto.
+"""
+class produtos:
+    """
+    Representa os produtos do estoque.
+    """
